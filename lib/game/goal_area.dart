d891e94ac2e9b057f47ed29f1b27204dbd04ef5b
mcs-cocacola <159092765+mcs-cocacola@users.noreply.github.com>
2026-05-04 18:27:05 +0800

    finished

--- lib/game/goal_area.dart
+++ lib/game/goal_area.dart
@@ -1,6 +1,5 @@
 import 'package:flame/collisions.dart';
 import 'package:flame/components.dart';
-import 'package:flutter/material.dart';
 
 class GoalArea extends RectangleComponent {
   GoalArea(Vector2 position)
@@ -8,7 +7,6 @@ class GoalArea extends RectangleComponent {
           size: Vector2(1500, 60),
           position: position,
           anchor: Anchor.center,
-          paint: Paint()..color = Colors.red.withValues(alpha: 0.5),
         );
 
   @override
